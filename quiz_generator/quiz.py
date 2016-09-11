fda3c7289eec0d6da3375e3fe25f699121855d32
Janis <janis@jaggedverge.com>
2016-09-11 21:19:15 +1000

    Implemented method to extract answers and made test

--- quiz_generator/quiz.py
+++ quiz_generator/quiz.py
@@ -2,7 +2,9 @@
 Representation of a quiz
 """
 
-default_quiz_template = """
+import jinja2
+
+default_quiz_template = jinja2.Template("""
 {{ quiz_name }} (version: {{ quiz_version }})
 {{ preamble }}
 
@@ -10,13 +12,21 @@ default_quiz_template = """
     Question #{{ loop.index }}:
     {{ question.question_to_latex }}
 {% endfor %}
-"""
+""")
+
+default_marking_sheet_template = jinja2.Template("""
+Marking sheet for {{ quiz_name }} version {{ quiz_version }}
+{% for answer in answers %}
+    Answer for question ${{loop.index}}: {{ answer }}
+{% endfor %}
+""")
 
 class Quiz:
     """Represents a Quiz"""
 
     def __init__(self, questions, quiz_name="", quiz_version="",
-                 preamble=None, template=default_quiz_template):
+                 preamble=None, quiz_questions_template=default_quiz_template,
+                 marking_sheet_template=default_marking_sheet_template):
         """
         :questions: A collection of questions that comprises this quiz
         :quiz_name: The name of this quiz
@@ -27,14 +37,25 @@ class Quiz:
         self.quiz_name = quiz_name
         self.quiz_version = quiz_version
         self.preamble = preamble
+        self.quiz_questions_template = quiz_questions_template
+        self.marking_sheet_template = marking_sheet_template
 
     def add_question(self, question):
         """Add a question to the Quiz"""
-        raise NotImplementedError
+        self.questions.append(question)
 
     def create_marking_sheet(self):
         """Create a marking sheet for this quiz"""
-        raise NotImplementedError
+        answers = []
+        for question in self.questions:
+            answers.append(question.answer_to_latex)
+
+        rendered_output = self.marking_sheet_template.render({
+            "quiz_name": self.quiz_name,
+            "quiz_version": self.quiz_version,
+            "answers": answers,
+        })
+        return rendered_output
 
     def quiz_to_latex(self):
         """Output the quiz in a LaTeX format"""
--- quiz_generator/tests/test_quiz.py
+++ quiz_generator/tests/test_quiz.py
@@ -14,3 +14,25 @@ def test_quiz_creation():
         quiz_version="Unit test version",
         preamble="Preamble, preamble, more preamble"
     )
+
+    question2 = Mock()
+    example_quiz.add_question(question2)
+
+def test_marking_sheet():
+    """Test marking sheet correctly made for a Quiz"""
+    answer_to_q_1 = "Answer to question1"
+    answer_to_q_2 = "Answer to question2"
+    question1 = Mock()
+    question1.answer_to_latex = answer_to_q_1
+    question2 = Mock()
+    question2.answer_to_latex = answer_to_q_2
+
+    name = "Mock quiz"
+    test_quiz = Quiz(
+        questions=[question1, question2],
+        quiz_name=name
+    )
+    marking_sheet = test_quiz.create_marking_sheet()
+    assert name in marking_sheet
+    assert answer_to_q_1 in marking_sheet
+    assert answer_to_q_2 in marking_sheet
