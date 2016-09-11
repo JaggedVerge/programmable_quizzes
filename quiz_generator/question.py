4b91e210714e7eb6eb06d1028da78fabeeff46b7
Janis <janis@jaggedverge.com>
2016-09-11 17:35:24 +1000

    Change input parameters

--- quiz_generator/question.py
+++ quiz_generator/question.py
@@ -4,12 +4,13 @@ Representation of a question.
 
 class Question:
     """Object that stores a question"""
-    def __init__(self, question_template, answer_template, answer_generation_function=None):
+    def __init__(self, question_template, answer_template, inputs=None, answer_generation_function=None):
         """
         :question_template: a template for the question
         :answer_template: a tempalte for the answer
+        :inputs: a dictionary of substitutions to make in the templates
         :answer_generation_function: a function that takes the variables found in the
-                                     question template and generates a dictionary of answer
+                                     question template and returns a dictionary of answer
                                      keys with values for use in the answer_template
         """
         self.question_template = question_template
