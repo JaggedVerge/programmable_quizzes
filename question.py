4c97c780b704c1dd70dc52181a704fe64abc9481
Janis <janis@jaggedverge.com>
2016-09-11 16:32:37 +1000

    Created init

--- question.py
+++ question.py
@@ -4,8 +4,16 @@ Representation of a question.
 
 class Question:
     """Object that stores a question"""
-    def __init__(self):
-        pass
+    def __init__(self, question_template, answer_template, answer_generation_function=None):
+        """
+        :question_template: a template for the question
+        :answer_template: a tempalte for the answer
+        :answer_generation_function: a function that takes the variables found in the
+                                     question template and generates a dictionary of answer
+                                     keys with values for use in the answer_template
+        """
+        self.question_template = question_template
+        self.answer_template = answer_template
 
     def question_to_latex(self):
         """Write out a representation of the question to LaTeX"""
