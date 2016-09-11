d481d41a97e085efaa7ad574ba2af3e2d52ad453
Janis <janis@jaggedverge.com>
2016-09-11 17:01:25 +1000

    Created setup.py file

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+import os
+
+setup(name='randomized_quiz_generator',
+      version='0.1.0',
+      description='Generates randomized quizzes from templates along with generated marking sheets',
+      url='https://github.com/shuttle1987/latex_quiz_generator',
+      packages=['quiz_generator'],
+      classifiers = [
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Education',
+          'Programming Language :: Python',
+      ],
+      keywords='',
+)
+
