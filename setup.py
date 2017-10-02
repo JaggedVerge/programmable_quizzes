ab971963a3a0381bbfdf2f7b6664f76425b316b3
Janis Lesinskis <footbagfreestyle@gmail.com>
2017-10-02 04:31:05 +0000

    Update setup URL

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 setup(name='randomized_quiz_generator',
       version='0.1.0',
       description='Generates randomized quizzes from templates along with generated marking sheets',
-      url='https://github.com/shuttle1987/latex_quiz_generator',
+      url='https://github.com/JaggedVerge/programmable_quizzes',
       packages=['quiz_generator'],
       classifiers = [
           'Development Status :: 3 - Alpha',
